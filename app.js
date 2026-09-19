4af0154f8e7969cf7cc59895a9374d9151853158
Alnino <18alnino@gmail.com>
2026-09-19 08:11:28 +0500

    Fix admin password hash and update version

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 
 // --- AUTHENTICATION LOGIC ---
-const ADMIN_HASH = "8c6976e5b5410415bde908bd4dee15dfb167a9c873fc4bb8a81f6f2ab448a918"; // admin123
+const ADMIN_HASH = "240be518fabd2724ddb6f04eeb1da5967448d7e831c08c8fa822809f74c720a9"; // admin123
 
 async function sha256(message) {
     const msgBuffer = new TextEncoder().encode(message);
