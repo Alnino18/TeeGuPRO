4311414be8babeef6c58291bedeb2c4922883766
Alnino <18alnino@gmail.com>
2026-09-22 17:46:23 +0500

    feat: unique icons for courier and agent apps

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE = 'salat-v2';
-const ASSETS = ['./index.html', './courier.html', './agent.html', './manifest.json', './manifest-courier.json', './manifest-agent.json'];
+const CACHE = 'salat-v3';
+const ASSETS = ['./index.html', './courier.html', './agent.html', './manifest.json', './manifest-courier.json', './manifest-agent.json', './courier-icon.jpg', './agent-icon.jpg'];
 
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
